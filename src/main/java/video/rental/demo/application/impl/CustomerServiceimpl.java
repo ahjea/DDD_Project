5565a84cf6099ddaed378e054ef3a211216cd46d
ahjea <pkdkswpals@gmail.com>
2018-06-16 22:01:02 +0900

    impl

--- src/main/java/video/rental/demo/application/impl/CustomerServiceimpl.java
+++ src/main/java/video/rental/demo/application/impl/CustomerServiceimpl.java
@@ -99,6 +99,25 @@ public class CustomerServiceimpl implements CustomerService {
 			}
 		}
 	}
+	/*
+	//duplicated
+	public void returnVideo(String customerCode, String videoTitle) {
+		Customer foundCustomer = getCustomerRepository().findCustomerById(new CustomerID(customerCode));
+		if (foundCustomer == null)
+			return;
 	
+		List<Rental> customerRentals = foundCustomer.getRentals();
 	
+		for (Rental rental : customerRentals) {
+			if (rental.getVideo().getTitle().equals(videoTitle) && rental.getVideo().isRented()) {
+				Video video = rental.returnVideo();
+				video.setRented(false);
+				getVideoRepository().saveVideo(video);
+				break;
+			}
+		}
+	
+		getCustomerRepository().saveCustomer(foundCustomer);
+	}
+	*/
 }
